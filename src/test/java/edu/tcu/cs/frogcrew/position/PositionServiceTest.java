57a741701d842c5507e56e2cafb41320ced98b64
Dylan Noall <dylannoall@outlook.com>
2025-04-23 11:41:01 -0500

    added starter methods to service tests

--- src/test/java/edu/tcu/cs/frogcrew/position/PositionServiceTest.java
+++ src/test/java/edu/tcu/cs/frogcrew/position/PositionServiceTest.java
@@ -1,4 +1,45 @@
 package edu.tcu.cs.frogcrew.position;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class PositionServiceTest {
+
+    @Mock
+    PositionRepository positionRepository;
+
+    @InjectMocks
+    PositionService positionService;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void testFindAllPositionNamesSuccess() {}
+
+    @Test
+    void testFindAllPositionNamesNotFound() {}
+
+    @Test
+    void testSaveSuccess() {}
+
+    @Test
+    void testUpdateSuccess() {
+    }
+
+    @Test
+    void testUpdateNotFound() {}
+
+    @Test
+    void testFindByPositionName() {}
+
+    @Test
+    void testFindByPositionNameNotFound() {}
 }
